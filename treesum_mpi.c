096a075a392cec04b542cf3bcf007924b25fea82
Travis Koski <travis_koski@umail.ucsb.edu>
2022-01-27 16:16:38 -0800

    fixed treesum

--- treesum_mpi.c
+++ treesum_mpi.c
@@ -32,39 +32,31 @@ int global_sum(int my_int /* in */, int my_rank /* in */, int comm_sz /* in */,
                MPI_Comm comm /* in */) {
     int my_sum = my_int;
     int other_sum;
-    int myPartner;
+    int partner;
     int div = 2; //used for pairing up processes
-    int doneComputing = 0;
+    int finished = 0;
     int gap = 1;
     //pre checks
+    int im_sending;
+    while(!finished && div<=nextPow2(comm_sz)){
+	    im_sending = my_rank % div;
+	    if(im_sending){
+	    partner = my_rank - gap;
+	    MPI_Send(&my_sum,1,MPI_INT,partner,0,comm);
+	    finished = 1;
+	    }
+	    else{
+	    partner = my_rank + gap;
+	    if(partner < comm_sz){
+		    MPI_Recv(&other_sum,1,MPI_INT, partner, 0, comm,MPI_STATUS_IGNORE);
+		    my_sum+=other_sum;
+	    }
+	    div*=2;
+	    gap*=2;
 
-
-    while(!doneComputing && div <= nextPow2(comm_sz))
-    {
-      if(my_rank % div == 1){
-        //sending
-        myPartner = my_rank - gap;
-        MPI_Send(&my_sum, 1, MPI_INT, myPartner,0, comm);
-        doneComputing = 1;
-      }
-      else{
-        //receiving
-        myPartner = my_rank + gap;
-        if(myPartner < comm_sz){
-          MPI_Recv(&other_sum, 1, MPI_INT, myPartner,0,comm,MPI_STATUS_IGNORE);
-          my_sum += other_sum;
-        }
-      }
-      div*=2;
-      gap*=2;
-
+	    }
     }
-  
-  
-  if(my_rank == 0){
+    return my_sum;
+    }
 
-  return my_sum;
-  }
-  return 1;
-               }
   /* Your solution */ /* Global_sum */
